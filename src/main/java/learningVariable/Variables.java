4937ce7a9dda8ddc1c87af49c413a91296e26f88
Irfan Yuksel <irfanyuksel@Irfans-MacBook-Pro.local>
2019-12-28 11:38:46 -0500

    first commit

---
+++ src/main/java/learningVariable/Variables.java
@@ -0,0 +1,33 @@
+package learningVariable;
+
+public class Variables {
+	public static void main(String[] args) {
+
+		String title = "Java";
+		
+		System.out.println(title);
+		
+		
+		String make = "Porsche";
+		
+		String model = "Cayenne";
+		
+		System.out.println("My next new car will be "+make+" "+model);
+		
+		
+		String firstName, lastName, job;
+		int age;
+		int salary;
+		int phoneNumber;
+		
+		///String a , int b;
+
+		boolean _ =true;
+		String $ = "USD";
+		String __________________24234234_____OK____________________DONE = "Testing";
+		
+		System.out.println(__________________24234234_____OK____________________DONE);
+		
+	
+	}
+}
