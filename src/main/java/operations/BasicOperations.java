8126b992985b8d1cea4c22fc75d6705273c6b148
Irfan Yuksel <irfanyuksel@Irfans-MacBook-Pro.local>
2019-12-28 12:30:54 -0500

    Added java operators

---
+++ src/main/java/operations/BasicOperations.java
@@ -0,0 +1,16 @@
+package operations;
+
+public class BasicOperations {
+	public static void main(String[] args) {
+		//https://www.w3schools.com/java/java_operators.asp
+		
+		int numA = 10;
+		int numB = 20;
+		
+		int result = numA + numB;
+		
+		System.out.println(result);
+		
+	
+	}
+}
