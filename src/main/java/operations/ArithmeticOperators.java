ddc6df64af17b5134d25048af1d1f35c87c33010
Irfan Yuksel <irfanyuksel@Irfans-MacBook-Pro.local>
2019-12-28 14:35:15 -0500

    Added java operators

---
+++ src/main/java/operations/ArithmeticOperators.java
@@ -0,0 +1,65 @@
+package operations;
+
+public class ArithmeticOperators {
+	
+
+	
+	public static void main(String[] args) {
+		
+		
+		//Reminder
+		int intNumber1 = 9;
+		
+		int intNumber2 = 20;
+		
+//		System.out.println(intNumber2 % intNumber1);
+		
+		int currentSecond = 58;
+		
+		if (currentSecond % 2 == 0 ) {
+			//Create IPR
+			System.out.println("Creating IPR");
+		}else {
+			//Create CBM
+			System.out.println("Creating CBM");
+		}
+		
+		//increament ++
+		
+		
+//		System.out.println("currentSecond at the line 30 : "+currentSecond);
+//		currentSecond ++;
+//		
+//		System.out.println("currentSecond at the line 33 : "+currentSecond);
+//		
+//		currentSecond --;
+//		System.out.println("currentSecond at the line 36 : "+currentSecond);
+//		
+//		currentSecond --;
+//		System.out.println("currentSecond at the line 39 : "+currentSecond);
+//		currentSecond --;
+//		System.out.println("currentSecond at the line 41 : "+currentSecond);
+//		
+//		
+		
+//		Assignment operators
+		
+		
+		intNumber1 +=intNumber2;  
+//		intNumber1 = intNumber1 + intNumber2;
+		
+		System.out.println(intNumber1);
+		
+		
+		int a = 5;
+		
+		a +=5;
+		a +=5;
+		a +=5;
+		System.out.println(a);
+		
+		
+		
+		
+	}
+}
--- src/main/java/operations/BasicOperations.java
+++ src/main/java/operations/BasicOperations.java
@@ -2,15 +2,62 @@ package operations;
 
 public class BasicOperations {
 	public static void main(String[] args) {
-		//https://www.w3schools.com/java/java_operators.asp
+		// https://www.w3schools.com/java/java_operators.asp
+
+		
+//		Arithmetic Operators
 		
 		int numA = 10;
 		int numB = 20;
-		
+
+		//addition 
 		int result = numA + numB;
-		
+
+		System.out.println(result);
+
+//		subtrction
+		result = numB - numA;
 		System.out.println(result);
 		
-	
+//		* /
+		
+		///Casting
+		double resultDouble = (double)numA/(double)numB;
+		
+		System.out.println(resultDouble);
+		
+		int numIntegerC = 4232327;
+		
+		byte numByteD = (byte)numIntegerC;
+		
+		System.out.println("Value of number d is : "+numByteD);
+		
+		
+		long longValue = 123345566;
+		
+		short shortValue = (short)longValue;
+		
+		System.out.println(longValue);
+		System.out.println(shortValue);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
